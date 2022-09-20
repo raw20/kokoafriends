56167e975ebbef1c6cc4916c09e2a2e8d0053cdd
정찬욱 <62588402+raw20@users.noreply.github.com>
2022-09-20 12:23:37 +0900

    img파일 경로 수정

--- front/src/components/gnb/Contents.tsx
+++ front/src/components/gnb/Contents.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function NewProductItem() {
+function Contents() {
   return <div>신상품</div>;
 }
 
-export default NewProductItem;
+export default Contents;
--- front/src/routes/Router.tsx
+++ front/src/routes/Router.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import KaKaoRedirect from "../auth/KaKaoRedirect";
 import Home from "../pages/Home";
-import NewProductItem from "../components/gnb/NewProductItem";
+import NewProductItem from "../components/gnb/Contents";
 import BestProductItem from "../components/gnb/BestProductItem";
 import Search from "../components/utilmenu/Search";
 
@@ -11,8 +11,8 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="/new" element={<NewProductItem />} />
           <Route path="/best" element={<BestProductItem />} />
+          <Route path="/contents" element={<NewProductItem />} />
           <Route path="/search" element={<Search />} />
         </Route>
         <Route path="/oauth/callback/kakao" element={<KaKaoRedirect />} />
