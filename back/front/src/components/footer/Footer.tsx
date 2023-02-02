f1acd5357f5f2cb7f78175a811513c9834f1c336
정찬욱 <raw1250@gmail.com>
2023-02-02 15:53:01 +0900

    refactor(components) Footer 초기 스타일링

--- back/front/src/components/footer/Footer.tsx
+++ back/front/src/components/footer/Footer.tsx
@@ -1,61 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-const Wrap = styled.div`
-  width: 100%;
-  height: 250px;
-  background-color: ${(props) => props.theme.boxColor};
-  color: ${(props) => props.theme.secondColor};
-  font-family: "Noto Sans KR", sans-serif;
-  box-sizing: border-box;
-`;
-const Inner = styled.div`
-  width: 30%;
-  height: auto;
-  margin: 0 auto;
-  @media ${(props) => props.theme.mobile} {
-    width: 100%;
-  }
-`;
-const FooterDiv = styled.div`
-  width: 100%;
-  padding-top: 2rem;
-  display: flex;
-  @media ${(props) => props.theme.mobile} {
-    width: 80%;
-    margin: 0 auto;
-  }
-`;
-const Text = styled.p`
-  width: 200px;
-  font-size: 1rem;
-  font-weight: 300;
-  margin-right: 0.5rem;
-  @media ${(props) => props.theme.mobile} {
-    font-size: 0.5rem;
-  }
-`;
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
 function Footer() {
   return (
-    <Wrap>
-      <Inner>
-        <FooterDiv>
-          <Text>Kokoa</Text>
-        </FooterDiv>
-        <FooterDiv>
-          <Text>포로젝트 명</Text>
-          <Text>코코아 프렌즈샵</Text>
-        </FooterDiv>
-        <FooterDiv>
-          <Text>조원</Text>
-          <Text>정찬욱 박민규</Text>
-        </FooterDiv>
-        <FooterDiv>
-          <Text>깃 주소</Text>
-          <Text>https://github.com/raw20/kokoafriends</Text>
-        </FooterDiv>
-      </Inner>
-    </Wrap>
+    <>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </>
   );
 }
 
