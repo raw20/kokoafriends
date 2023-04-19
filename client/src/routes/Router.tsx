c5facb1bc88a5fee82de91430f9a592c5455b6ed
정찬욱 <raw1250@gmail.com>
2023-04-19 19:59:02 +0900

    refactor: Router 코드 분할

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,41 +1,51 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Main/Home/Home";
-import Contents from "../pages/Main/Contents/Contents";
-import BestProducts from "../pages/Main/Best/BestProducts";
-import Search from "../pages/Sub/Search/Search";
-import Product from "../pages/Main/Best/Product";
-import Mypage from "../pages/Sub/Mypage/Mypage";
-import Cart from "../pages/Sub/Cart/Cart";
-import KaKaoLoginRedirect from "../services/auth/KaKaoLoginRedirect";
-import KaKaoLogoutRedirect from "../services/auth/KaKaoLogoutRedirect";
-import BuyCheckout from "../pages/Sub/BuyCheckout/BuyCheckout";
+import Loading from "../components/Loading/Loading";
+
+const Home = lazy(() => import("../pages/Main/Home/Home"));
+const Contents = lazy(() => import("../pages/Main/Contents/Contents"));
+const BestProducts = lazy(() => import("../pages/Main/Best/BestProducts"));
+const Search = lazy(() => import("../pages/Sub/Search/Search"));
+const Product = lazy(() => import("../pages/Main/Best/Product"));
+const Mypage = lazy(() => import("../pages/Sub/Mypage/Mypage"));
+const Cart = lazy(() => import("../pages/Sub/Cart/Cart"));
+const BuyCheckout = lazy(() => import("../pages/Sub/BuyCheckout/BuyCheckout"));
+
+const KaKaoLoginRedirect = lazy(
+  () => import("../services/auth/KaKaoLoginRedirect")
+);
+const KaKaoLogoutRedirect = lazy(
+  () => import("../services/auth/KaKaoLogoutRedirect")
+);
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          {/* main menu */}
-          <Route path="/best" element={<BestProducts />} />
-          <Route path="/contents" element={<Contents />} />
-          <Route path="/product/:id" element={<Product />} />
-          {/* util menu */}
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/search" element={<Search />} />
-          {/* login logout*/}
-          <Route
-            path="/oauth/callback/kakao"
-            element={<KaKaoLoginRedirect />}
-          />
-          <Route
-            path="/oauth/callback/kakao/logout"
-            element={<KaKaoLogoutRedirect />}
-          />
-          {/* Order Page */}
-          <Route path="/checkout" element={<BuyCheckout />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {/* main menu */}
+            <Route path="/best" element={<BestProducts />} />
+            <Route path="/contents" element={<Contents />} />
+            <Route path="/product/:id" element={<Product />} />
+            {/* util menu */}
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/search" element={<Search />} />
+            {/* login logout*/}
+            <Route
+              path="/oauth/callback/kakao"
+              element={<KaKaoLoginRedirect />}
+            />
+            <Route
+              path="/oauth/callback/kakao/logout"
+              element={<KaKaoLogoutRedirect />}
+            />
+            {/* Order Page */}
+            <Route path="/checkout" element={<BuyCheckout />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
