ecbd26d3e64b282d42dd29747693710a0aff86d2
정찬욱 <raw1250@gmail.com>
2023-04-21 00:46:28 +0900

    refactor: 빈 장바구니시 나오는 컴포넌트 리팩토링 && AlertPage.Common.style 생성으로 Login,EmptyCart 스타일 공통 관리

--- client/src/pages/Sub/Cart/EmptyCart.tsx
+++ client/src/pages/Sub/Cart/EmptyCart.tsx
@@ -1,61 +1,37 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-const Wrap = styled.div`
-  width: 100%;
-`;
-const Inner = styled.div`
-  width: 50%;
-  height: auto;
-  margin: 5rem auto;
-`;
-const Alert = styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
-const Image = styled.img`
-  width: 150px;
-  height: 150px;
-  margin: 0 auto;
-`;
-const Text = styled.span`
-  width: 100%;
-  font-size: 1.1rem;
-  line-height: 1.8rem;
-  font-weight: 500;
-  text-align: center;
-  margin-top: 1.3rem;
-  color: ${(props) => props.theme.secondTextColor};
-`;
-const Button = styled(Link)`
-  width: 250px;
-  height: 40px;
-  color: ${(props) => props.theme.bgColor};
-  background-color: ${(props) => props.theme.secondTextColor};
-  border-radius: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 4rem auto;
-  cursor: pointer;
-`;
+import { useNavigate } from "react-router-dom";
+import {
+  AlertPageBox,
+  AlertPageButton,
+  AlertPageCaontainer,
+  AlertPageImage,
+  AlertPageInner,
+  AlertPageText,
+} from "../../../styles/AlertPage.Common.style";
+import { Stack } from "@mui/material";
+
 function EmptyCart() {
+  const navigator = useNavigate();
   return (
-    <Wrap>
-      <Inner>
-        <Alert>
-          <Image src={"/img/etc/lion_heart_etc.jpg"} alt="사진" />
-          <Text>
-            아직 관심상품이 없네요!
-            <br />
-            귀여운 프렌즈 상품을 추천드릴게요
-          </Text>
-          <Button to="/best">인기상품 보기</Button>
-        </Alert>
-      </Inner>
-    </Wrap>
+    <AlertPageCaontainer>
+      <AlertPageInner>
+        <AlertPageBox>
+          <Stack>
+            <AlertPageImage
+              src={require("../../../asset/image/etc/ico_cart_empty.png")}
+              alt="사진"
+            />
+            <AlertPageText>
+              아직 관심상품이 없네요!
+              <br />
+              귀여운 프렌즈 상품을 추천드릴게요
+            </AlertPageText>
+            <AlertPageButton onClick={() => navigator("/best")}>
+              인기상품 보기
+            </AlertPageButton>
+          </Stack>
+        </AlertPageBox>
+      </AlertPageInner>
+    </AlertPageCaontainer>
   );
 }
 
--- client/src/pages/Sub/Login/Login.tsx
+++ client/src/pages/Sub/Login/Login.tsx
@@ -1,58 +1,35 @@
-import styled from "styled-components";
+import { Stack } from "@mui/material";
 import { KAKAO_AUTH_URL } from "../../../constant/oAuth";
+import {
+  AlertPageCaontainer,
+  AlertPageInner,
+  AlertPageBox,
+  AlertPageImage,
+  AlertPageText,
+  AlertPageButton,
+} from "../../../styles/AlertPage.Common.style";
+import "../../../asset/image/etc";
 
-const Wrap = styled.div`
-  width: 100%;
-`;
-const Inner = styled.div`
-  width: 50%;
-  height: auto;
-  margin: 5rem auto;
-`;
-const Alert = styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
-const Image = styled.img`
-  width: 200px;
-  height: 200px;
-  margin: 0 auto;
-`;
-const Text = styled.span`
-  width: 100%;
-  font-size: 1.2rem;
-  text-align: center;
-  margin-top: 1.3rem;
-`;
-const Button = styled.span`
-  width: 250px;
-  height: 40px;
-  color: ${(props) => props.theme.bgColor};
-  background-color: ${(props) => props.theme.jaygColor};
-  border-radius: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 4rem auto;
-`;
 function Login() {
-  const image = ["choonsik_etc.png", "lion_etc.png"];
-  const index = Math.floor(Math.random() * image.length);
   return (
-    <Wrap>
-      <Inner>
-        <Alert>
-          <Image src={`/img/etc/${image[index]}`} alt="사진" />
-          <Text>로그인이 필요한 서비스입니다. 로그인 후 이용해 주세요.</Text>
-          <a href={KAKAO_AUTH_URL}>
-            <Button>카카오 로그인</Button>
-          </a>
-        </Alert>
-      </Inner>
-    </Wrap>
+    <AlertPageCaontainer>
+      <AlertPageInner>
+        <AlertPageBox>
+          <Stack>
+            <AlertPageImage
+              src={require("../../../asset/image/etc/img_apeach.png")}
+              alt="사진"
+            />
+            <AlertPageText>
+              로그인이 필요한 서비스입니다. 로그인 후 이용해 주세요.
+            </AlertPageText>
+            <a href={KAKAO_AUTH_URL}>
+              <AlertPageButton>카카오 로그인</AlertPageButton>
+            </a>
+          </Stack>
+        </AlertPageBox>
+      </AlertPageInner>
+    </AlertPageCaontainer>
   );
 }
 
