19eeff72392c05f9b72ef5d162dd130cf76b3461
정찬욱 <raw1250@gmail.com>
2023-04-23 04:16:32 +0900

    chore: apollo-server uri 수정

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "http://3.36.249.183:4000/",
   cache: new InMemoryCache(),
 });
 
